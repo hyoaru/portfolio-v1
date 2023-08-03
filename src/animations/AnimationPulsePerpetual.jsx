c2139257850f340ba2e1ec297c0b43a790434fce
hyoaru <jjcabreraaaa@gmail.com>
2023-08-03 16:29:27 +0800

    [modify] pulse animation keyframes

--- src/animations/AnimationPulsePerpetual.jsx
+++ src/animations/AnimationPulsePerpetual.jsx
@@ -5,8 +5,8 @@ function AnimationPulsePerpetual(props) {
     return (
         <>
             <motion.div
-                animate={{ opacity: [0, 0.5, 1, 0.5, 0] }}
-                transition={{ repeat: Infinity, duration: 1.3, ease: "easeInOut" }}
+                animate={{ opacity: [0.1, 0.3, 0.5, 0.7, 1, 0.7, 0.5, 0.3, 0.1] }}
+                transition={{ repeat: Infinity, duration: 2, ease: "easeInOut" }}
             >
                 {props.children}
             </motion.div>
